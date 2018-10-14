7440f2c4c425c8487697a7b03e212517c93aa125
Mike <mike.dellagana@turntown.com>
2018-10-14 15:25:46 +0100

    Remove md bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +14,6 @@ import { HttpClientModule } from '@angular/common/http';
 		BrowserModule,
 		HttpClientModule,
 		AppRoutingModule,
-		MDBBootstrapModule.forRoot(),
 		HomeModule,
 	],
 	providers: [],
