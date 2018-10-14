114a92702b9e9700637964886dbb3b190b4d7a95
Mike <mike.dellagana@turntown.com>
2018-10-14 21:19:00 +0100

    Add basic controls to the camera in the map component. TODO: move picture code to the picture compoent

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -4,11 +4,13 @@ import { MapComponent } from './map.component';
 import { AgmCoreModule } from '@agm/core';
 import { MomentService } from '../core/moment.service';
 import { WebcamModule } from 'ngx-webcam';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     WebcamModule,
+    FormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyD1U1IGXORTC5dLCwiS1GHpYYZEcoAV7fs'
     })
